3ccb4b3c43ca44233a2c2f844e8a22c77c5a335e
MayaBrito <mayara.pinheiro@ccc.ufcg.edu.br>
2023-05-16 11:09:01 -0300

    Adicionando o inicio do desenvolvimento do roteiro 5

---
+++ Mayara-Brito_Roteiro5/Mayara-ContadorUpDown.sv
@@ -0,0 +1,29 @@
+//Aluna: Mayara Brito
+//Matricula: 121110615
+//Disciplina: Laboratório de Organização e Arquitetura de Computadores
+//Roteiro 5 ---> Problema 1 – Contador Up/Down
+//---------------------------------------------------------------------
+
+parameter BITS = 4;
+
+module contadorUpDown(
+	input logic reset, clk, sel,
+  output logic [BITS-1:0]count);
+  
+  initial begin
+    count <= 0;
+  end
+		
+  always_ff @(posedge reset or posedge clk) begin
+     
+    if (reset)
+      count <= 0; 
+    else begin 
+      if (sel) 
+        count <= count + 1; // UP
+      else
+        count <= count - 1; // DOWN
+    end 
+  end 
+      
+endmodule
