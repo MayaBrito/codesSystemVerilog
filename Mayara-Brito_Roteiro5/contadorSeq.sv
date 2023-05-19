6e220d57125f1f15a1311d825422473869beb7ea
MayaBrito <mayara.pinheiro@ccc.ufcg.edu.br>
2023-05-19 08:47:16 -0300

    Contador de Sequencia - Problema2 (Roteiro 5)

---
+++ Mayara-Brito_Roteiro5/contadorSeq.sv
@@ -0,0 +1,30 @@
+//Aluna: Mayara Brito
+//Matricula: 121110615
+//Disciplina: Laboratório de Organização e Arquitetura de Computadores
+//Roteiro 5 ---> Problema 2 – Contador de uma Sequência Qualquer
+//---------------------------------------------------------------------
+
+parameter BITS_COUNT = 4;
+
+module contador8421 (
+  input logic clk,reset, load,
+  input logic [BITS_COUNT-1:0] data_in,
+  output logic [BITS_COUNT-1:0] count);
+  
+  always_ff @(posedge clk) begin
+    if(reset)
+      count <= 0;
+    else if(load)
+      count <= data_in;
+    else begin
+      unique case (count) 
+        1: count <= 2;
+        2: count <= 4;
+        4: count <= 8;
+        8: count <= 1;
+      endcase
+    end
+  end
+  
+endmodule
+  
