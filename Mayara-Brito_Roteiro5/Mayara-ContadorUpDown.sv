b957a8a5b14238d04e00eaeccee612889d690276
Maya Brito <41126948+MayaBrito@users.noreply.github.com>
2023-06-01 16:39:39 -0300

    Update Mayara-ContadorUpDown.sv
    Atualizando o código com comentários.

--- Mayara-Brito_Roteiro5/Mayara-ContadorUpDown.sv
+++ Mayara-Brito_Roteiro5/Mayara-ContadorUpDown.sv
@@ -4,6 +4,13 @@
 //Roteiro 5 ---> Problema 1 – Contador Up/Down
 //---------------------------------------------------------------------
 
+// BITS --> Representa a quantidade de bits que meu contador vai receber, nesse caso o contador está funcionando de
+// [0:15] porque está sendo iniciado com 4 bits, dessa forma pode ser alterado conforme a escolha dos limites de contagem.
+
+// sel --> Representa a ação de incremento ou decremento do contador. 
+	// (sel = 1) o contador funcionará de maneira crescente.
+	// (sel = 0) o contador funcionará de maneira descrescente.
+
 parameter BITS = 4;
 
 module contadorUpDown(
