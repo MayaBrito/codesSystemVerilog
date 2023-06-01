e57242ec48db598951419b689fc10758e342e917
Maya Brito <41126948+MayaBrito@users.noreply.github.com>
2023-06-01 15:40:05 -0300

    Update Mayara-DetectorParidade.sv

--- Mayara-Brito_Roteiro5/Mayara-DetectorParidade.sv
+++ Mayara-Brito_Roteiro5/Mayara-DetectorParidade.sv
@@ -3,29 +3,36 @@
 //Disciplina: Laboratório de Organização e Arquitetura de Computadores
 //Roteiro 5 ---> Problema 3 – Detector de Paridade Par
 //---------------------------------------------------------------------
+
+// Size --> Tamanho que define os estados possiveis 
+// PARIDADE_VALIDA --> É quando a sequência lida possui uma quantidade par de bits iguais a 1
+// PARIDADE_INVALIDA --> É quando a sequência lida possui uma quantidade impar de bits iguais a 1, 
+			//nesse caso tornando inválida a paridade esperada que nesse caso seria paridade par. 
+
 parameter size = 1;
+
 module detector_paridade_par(
   input clk, reset, in_bit, 
   output out_bit);
   
-  enum logic [size - 1 : 0] {FALSE, TRUE} state;
+  enum logic [size - 1 : 0] {PARIDADE_VALIDA, PARIDADE_INVALIDA} state;
     
   always_ff @ (posedge clk)
-    if (reset) state <= TRUE;
+    if (reset) state <= PARIDADE_INVALIDA;
      else
 		unique case (state)
-			FALSE:
+			PARIDADE_VALIDA:
               if (in_bit == 1)
-					state <= TRUE;
+					state <= PARIDADE_INVALIDA;
 				else 
-					state <= FALSE;
-			TRUE:
+					state <= PARIDADE_VALIDA;
+			PARIDADE_INVALIDA:
               	if (in_bit == 1)
-					state <= FALSE;
+					state <= PARIDADE_VALIDA;
 				else
-					state <= TRUE;
+					state <= PARIDADE_INVALIDA;
 		endcase
 
-  always_comb out_bit <= (state == TRUE);
+  always_comb out_bit <= (state == PARIDADE_INVALIDA);
  
 endmodule 
